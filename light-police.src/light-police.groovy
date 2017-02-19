d21e82a7afbbc70cf6bc4276d92832c1c5bc8894
Alex Batlin <alex@batlin.com>
2017-02-19 22:27:06 +0000

    Update light-police.groovy

--- light-police.src/light-police.groovy
+++ light-police.src/light-police.groovy
@@ -17,7 +17,7 @@ definition(
     name: "Light Police",
     namespace: "alex.batlin",
     author: "Alex Batlin",
-    description: "Turns off lights every X minutes if Lux is Y",
+    description: "Turns off selected lights every selected number of minutes if sensor lux level is equal to or greater than set value.",
     category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
@@ -25,21 +25,11 @@ definition(
 
 
 preferences {
-	section("Select which lights should be switched how often if light level is above threshold.") {
+	section("Select which lights should be switched off how often when light level is above specified threshold.") {
 		input "bulbs","capability.switch", multiple: true, title: "Which light bulbs?"
         input "lightSensor", "capability.sensor", title: "Which light sensor?"
-        input name: "lux", type: "integer", title: "Daylight lux level?"
-        input (
-        	name: "interval", 
-            type: "enum", 
-            title: "Patrol interval?", 
-            options: 
-                [["5":"5 Minutes"],
-                ["10":"10 Minutes"],
-                ["15":"15 Minutes"],
-                ["30":"30 Minutes"],
-                ["60":"1 Hour"],
-                ["180":"3 Hours"]])
+        input name: "triggerLux", type: "decimal", title: "Lux minimum value?"
+        input name: "interval", type: "enum", title: "Patrol interval?", options: ["5 Minutes", "10 Minutes", "15 Minutes", "30 Minutes", "1 Hour", "3 Hours"]
 	}
 }
 
@@ -57,51 +47,52 @@ def updated() {
 	initialize()
 }
 
-def initialize() {    
-	log.debug "looking to set interval schedule to ${interval}"
+def initialize() {        
     switch(interval) {
-    	case "5":
-        	log.debug "setting interval schedule to 5 minutes"
+    	case "5 Minutes":
+        	log.debug "Setting interval schedule to: 5 minutes"
         	runEvery5Minutes(policeAction)
             break
-        case "10":
-        	log.debug "setting interval schedule to 10 minutes"
+        case "10 Minutes":
+        	log.debug "Setting interval schedule to: 10 minutes"
         	runEvery10Minutes(policeAction)
             break
-        case "15":
-        	log.debug "setting interval schedule to 15 minutes"
+        case "15 Minutes":
+        	log.debug "Setting interval schedule to: 15 minutes"
         	runEvery15Minutes(policeAction)
             break
-        case "30":
-        	log.debug "setting interval schedule to 30 minutes"
+        case "30 Minutes":
+        	log.debug "Setting interval schedule to: 30 minutes"
         	runEvery30Minutes(policeAction)
             break    
-        case "60":
-        	log.debug "setting interval schedule to 1 hour"
+        case "1 Hour":
+        	log.debug "Setting interval schedule to: 1 hour"
         	runEvery1Hour(policeAction)
             break
-        case "180":
-        	log.debug "setting interval schedule to 3 hours"
+        case "3 Hours":
+        	log.debug "Setting interval schedule to: 3 hours"
         	runEvery3Hours(policeAction)
             break            
 		default :
-        	log.debug "setting interval schedule to default 5 minutes"
+        	log.debug "Setting default interval schedule to: 5 minutes"
         	runEvery5Minutes(policeAction)
     }
 }
 
 def policeAction() {    
-    def currentState = lightSensor.currentState("illuminance")
-    def currentLux = currentState.value
-    log.debug "lux readout: ${currentLux}"
+	def currentLux = lightSensor.currentIlluminance
+    log.debug "Patrol settings: currentLux=${currentLux}, triggerLux=${triggerLux}"
     
-    if (currentLux >= lux) {
+    if (currentLux >= triggerLux) {
+    	log.debug "it's sunny so time to switch off lights" 
         bulbs?.each {
-            log.debug "${it.id} value ${it.currentSwitch}" 
+            log.debug "${it.name} value ${it.currentSwitch}" 
 
             if (it.currentSwitch == "on"){
                 it.off();
             }
         }
+    } else {
+    	log.debug "it's dark so keep those lights on" 
     }
 }
